e9f0b2dadaefd1dc72cf67aaae876e87d3183d91
Jennifer <daon0488@gmail.com>
2024-01-25 15:11:38 -0500

    first commit

---
+++ practice1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class practice1 {
+     
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+     System.out.println("Hello");  
+     scanner.close();
+  }
+    }   
